2163ed069d52d19386dc342d0c37cecc6884160e
Brenda Mota <brenda4158@gmail.com>
2023-04-20 19:27:59 -0300

    Criando exercício de soma

--- Entendendo o DOM/script.js
+++ Entendendo o DOM/script.js
@@ -1,2 +1,21 @@
-var p1 = window.document.getElementsByTagName('p')
-window.document.write()
+var a = window.document.getElementById('box')
+a.addEventListener("click", clicar)
+a.addEventListener("mouseenter", entrar)
+a.addEventListener("mouseout", sair)
+
+
+function clicar() {
+    a.innerText = 'Clicou';
+    a.style.background = 'red'
+}
+
+function entrar() {
+    a.innerText = 'Entrou';
+    a.style.background = 'purple'
+}
+
+function sair() {
+    a.innerText = 'Saiu';
+    a.style.background = 'green'
+}
+
